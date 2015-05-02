662f5ec1dd56078c6225b393bbb0b87dac3b3e0d
Mathieu Lajugie <mathieu@caring.com>
2015-05-02 09:40:34 -0700

    show pretty empty task

--- app/main/controllers/daily_controller.rb
+++ app/main/controllers/daily_controller.rb
@@ -7,5 +7,9 @@ module Main
     def edit_url(section)
       url_with(action: 'edit', section_id: section._id)
     end
+
+    def pluralize(count, word)
+      "#{count} #{count == 1 ? word : "#{word}s"}"
+    end
   end
 end
