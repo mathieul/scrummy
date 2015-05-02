2a054e23f5b2015629686e20e1cc34ce93128689
Mathieu Lajugie <mathieu@caring.com>
2015-05-01 20:49:50 -0700

    pass url as an attribute to the components

--- app/daily_section_viewer/controllers/main_controller.rb
+++ app/daily_section_viewer/controllers/main_controller.rb
@@ -1,6 +1,11 @@
-module DailySectionView
+module DailySectionViewer
   class MainController < Volt::ModelController
     def index
+    end
+
+    def index_ready
+      found = Element.find(container)
+      %x{console.log(found);}
     end
 
     def edit
